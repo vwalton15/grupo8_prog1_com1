f7ba724d236fd2b28aa62a9adc4e6a8f17e4cc31
MilagrosGalarce <galarcem@udesa.edu.ar>
2024-11-18 11:23:08 -0300

    Valide el frmulario de login

---
+++ java/login.js
@@ -0,0 +1,25 @@
+
+
+const formulario = document.querySelector("#login-form")
+const email = document.querySelector("#email")
+const contraseña = document.querySelector("#password")
+
+formulario.addEventListener("submit", function (event) {
+    event.preventDefault(); 
+
+    let verdad = true
+
+    if(email.value === ""){
+        alert("Por favor complete el campo email");
+        verdad = false;
+    }
+    if(contraseña.value == ""){
+        alert("Por favor complete el campo contraseña");
+        verdad = false;
+    }
+
+    if(verdad){
+        formulario.action = "index.html";
+        formulario.submit();
+    }
+})

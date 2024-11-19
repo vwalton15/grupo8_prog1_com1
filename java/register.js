f0579b5326bb99dae33efc8b0be944e1bc77e20e
MilagrosGalarce <galarcem@udesa.edu.ar>
2024-11-19 12:45:18 -0300

    arranque a validar el formulario de registro

---
+++ java/register.js
@@ -0,0 +1,13 @@
+const formulario_reegistro = document.querySelector("#register-form");
+const email_r = document.querySelector("#email")
+const contra_r = document.querySelector("#password")
+
+const error_email = document.querySelector("#email-error")
+const error_contra = document.querySelector("#password-error")
+
+formulario_registro.addEventListener("submit", function (event) {
+    event.preventDefault(); 
+    
+
+
+})

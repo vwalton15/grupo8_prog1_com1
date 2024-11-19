c65d0b6cb19ceb44d9cc77c9315b8c6843aca608
vwalton15 <vwalton@udesa.edu.ar>
2024-11-19 16:22:28 -0300

    Avance con la pagina receta y su js

--- java/receta.js
+++ java/receta.js
@@ -1,27 +1,29 @@
 let queryString = window.location.search;
 let obj = new URLSearchParams(queryString);
-let id = obj.get("id")
+let id = obj.get("id");
 
- let url = `https://dummyjson.com/recipes/${id}`;
-fetch (url)
-.then(function (response){
-	return response.json();
+let url = `https://dummyjson.com/recipes/${id}`;
+
+fetch(url)
+    .then(function (response){
+	    return response.json()
 })
-.then(function (data){
-        let nombre = document.querySelector(".nombre-receta")
-        let img = document.querySelector(".img-receta")
-        let tiempo = document.querySelector(".tiempo-receta")
-        let categoria = document.querySelector(".categoria-receta")
-        let instrucciones = document.querySelector(".instrucciones-receta")
+    .then(function (data){
+        console.log(data)
+        let nombre = document.querySelector("#nombre-receta")
+        let imagen = document.querySelector("#img-receta")
+        let tiempo = document.querySelector("#tiempo-receta")
+        let categoria = document.querySelector("#categoria-receta")
+        let instrucciones = document.querySelector("#instrucciones-receta")
     
         nombre.innerHTML += `${data.nombre}`
-        img.innerHTML += `${data.img}`
+        imagen.src = `${data.img}`
         tiempo.innerHTML += `${data.tiempo}`
         categoria.innerHTML += `${data.categoria}`
         instrucciones.innerHTML += `${data.instrucciones}`
     
 })
-.catch(function(error){
-	console.log('El error es: ' + error);
+    .catch(function(error){
+	    console.log(error);
 })
 

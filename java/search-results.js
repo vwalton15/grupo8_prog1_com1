735c6fc2dd2e65089632fa4432f37d86d0b4bda0
MilagrosGalarce <galarcem@udesa.edu.ar>
2024-11-23 18:26:14 -0300

    Avance con las apis del search

--- java/search-results.js
+++ java/search-results.js
@@ -1,11 +1,37 @@
-let queryStrin = location.search;
-let obj = new URLSearchParams(querystring);
+let queryString = location.search;
+let obj = new URLSearchParams(queryString);
 let palabra_Buscada = obj.get("search");
+console.log(palabra_Buscada)
 
 fetch(`https://dummyjson.com/recipes/search?q=${palabra_Buscada}`)
    .then(function(response){
        return response.json();
    })
 
+   .then(function (data){
+      console.log(data.recipes);
 
+     let result = document.querySelector(".result");
 
+     if (!palabra_Buscada) {
+        result.innerText = "La búsqueda está vacía"
+        return; 
+     }
+
+     if (!data.recipes || data.recipes.length === 0){
+        result.innerText = "No hay ninguna reecta";
+     } else{
+        let resultados = [];
+
+        for (let i = 0; i < data.recipes.length; i++) {
+            let res = data.recipes[i];
+            resultados.push(res)
+            
+        }
+
+        if (resultados.length === 0){
+            result.innerText = `No existen resultados relacionados con tu búsqueda: "${palabra_Buscada}`;
+
+        } }
+   })
+    

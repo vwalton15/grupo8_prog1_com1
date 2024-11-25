cbef1ae4aceb3d7766f95390e4e94f0d9a35a963
vwalton15 <vwalton@udesa.edu.ar>
2024-11-25 19:24:01 -0300

    Arregle css, hice el js de category

--- java/category.js
+++ java/category.js
@@ -2,25 +2,40 @@ let queryString = window.location.search;
 let parametro = new URLSearchParams(queryString);
 let categoria = parametro.get("tag");
 
+let nombreCategoria = document.querySelector(".categoria-nombre")
+nombreCategoria.textContent = `Categoria: ${categoria}`
+
 fetch (`https://dummyjson.com/recipes`)
     .then(function(response){
 	return response.json();
 })
 	.then(function(data){
-	console.log(data);
-
     const recetas = data.recipes;
     const ListaRecetas = document.querySelector(".lista-categorias")
     let receta = "";
 
-    for (let i = 0; i < recipes.length; i++) {
-        const recetario = recipes[i];
+    for (let i = 0; i < recetas.length; i++) {
+        const recetario = recetas[i];
         
-        if (receta.tags && receta.tags) {
-            
-        }
+        if (recetario.tags){
+            for (let index = 0; index < recetario.tags.length; index++) {
+                if (recetario.tags[index] === categoria) {
+                    ListaRecetas.innerHTML +=
+                        ` <li>
+                            <h2 class="categoria-titulo">${recetario.name}</h2>
+                            <img class="categoria-img" src="${recetario.image}" alt="${recetario.name}">
+                            <p class="categoria-dificultad">Difficulty: ${recetario.difficulty}</p>
+                            <a href="./receta.html?id=${recetario.id} class="categoria-link">Ver detalle</a>
+                        </li>`
+                }
+                
+            }
+        }  
+        else {
+            ListaRecetas.innerHTML =  "<p>No hay recetas disponibles para esta categoria</p>"
+            }  
     }
 })
 	.catch(function(error){
-	console.log('El error es: ' + error);
+	console.log(error);
 })

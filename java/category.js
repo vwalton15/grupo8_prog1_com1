8c5ad44907773f01753619d11fe6b345b592d2a0
vwalton15 <vwalton@udesa.edu.ar>
2024-11-26 08:43:22 -0300

    Arregle category

--- java/category.js
+++ java/category.js
@@ -2,24 +2,21 @@ let queryString = window.location.search;
 let parametro = new URLSearchParams(queryString);
 let categoria = parametro.get("tag");
 
+const ListaRecetas = document.querySelector(".lista-categorias")
+
 let nombreCategoria = document.querySelector(".categoria-nombre")
 nombreCategoria.textContent = `Categoria: ${categoria}`
 
-fetch (`https://dummyjson.com/recipes`)
+fetch (`https://dummyjson.com/recipes/tag/${categoria}`)
     .then(function(response){
 	return response.json();
 })
 	.then(function(data){
     const recetas = data.recipes;
-    const ListaRecetas = document.querySelector(".lista-categorias")
     let receta = "";
-
+    
     for (let i = 0; i < recetas.length; i++) {
         const recetario = recetas[i];
-        
-        if (recetario.tags){
-            for (let index = 0; index < recetario.tags.length; index++) {
-                if (recetario.tags[index] === categoria) {
                     ListaRecetas.innerHTML +=
                         ` <li>
                             <h2 class="categoria-titulo">${recetario.name}</h2>
@@ -30,12 +27,7 @@ fetch (`https://dummyjson.com/recipes`)
                 }
                 
             }
-        }  
-        else {
-            ListaRecetas.innerHTML =  "<p>No hay recetas disponibles para esta categoria</p>"
-            }  
-    }
-})
+    )
 	.catch(function(error){
 	console.log(error);
 })
